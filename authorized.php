446290835b1c49efdde67ce8120a9f7a70dbccd1
taung2630 <genius.thura@gmail.com>
2015-01-04 17:04:23 +0800

    Create authorized.php

---
+++ authorized.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+session_start();
+echo $_SESSION['login'];
+echo $_SESSION['username'];
+echo $_SESSION['msg'];
+?>
+</body>
+</html>
